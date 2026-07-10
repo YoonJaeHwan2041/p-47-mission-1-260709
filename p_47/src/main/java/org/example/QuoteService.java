6eaf7233631a42a11934786c04822e788845c695
YoonJaeHwan2041 <jkl0379@naver.com>
2026-07-10 12:15:26 +0900

    chore : 쓰지않는 Scanner제거

--- p_47/src/main/java/org/example/QuoteService.java
+++ p_47/src/main/java/org/example/QuoteService.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 // 앞에 접근 제어자가 없는 이유는 같은 패키지 안에 있어서 접근 제어자를 생략했다.
 // 추후 도메인별로 분리하여 패키지가 변경될 시에 앞에 접근제어자를 붙여야 할 경우가 많아 질것이다.
 public class QuoteService {
-    Scanner sc = new Scanner(System.in);
     ArrayList<Quote> quotes = new ArrayList<>();
 
     // 등록
